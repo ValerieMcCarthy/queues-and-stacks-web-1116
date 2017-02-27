b364e225e97de24594f22625f775b4bcc202d9f6
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2017-02-27 10:36:59 -0500

    lined up

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,4 +2,17 @@ require_relative 'stack'
 
 class Queue
 
+	def initialize
+		@store = []
+	end
+
+	def enqueue(x)
+		@store << x
+	end
+
+	def dequeue
+		@store.shift
+	end
+
+
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,25 @@
 class Stack
 
+	def initialize
+		@store = []
+		@store_length = 0
+	end
+
+	def push(x)
+		@store.push(x)
+	end
+
+	def pop
+		@store.pop
+	end
+
+	def empty?
+		@store.empty?
+	end
+
+	def size
+		@store_length = @store.length
+	end
+
+
 end
